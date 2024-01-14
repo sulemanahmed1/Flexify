0f2cdd76ca53cd81926fa08628d1a720be13a177
suleman ahmed <ahmedsuleman@outlook.dk>
2024-01-14 18:32:00 +0100

    styling, hero section image responsive

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -10,9 +10,9 @@ export default function Home() {
         <div className="container flex flex-col-reverse mx-auto p-6 lg:flex-row">
           <div className="flex flex-col space-y-10 mb-44 lg:mt-16 lg:w-1/2 xl:mb-52">
             <h1 className="text-5xl font-bold text-center lg:text-6xl lg:max-w-md lg:text-left text-[#7B5D47]">Find din næste freelancer</h1>
-            <p className="text-2xl text-center lg:max-w-md lg:text-left text-[#957B71] ">Talenter der skaber værdi for 
-            dit projekt, vælg blandt en bred vifte af kompetencer</p>
-            <Search placeholder='søg kategori eller '/>
+            <p className="text-2xl text-center lg:max-w-md lg:text-left text-[#957B71] ">Talenter der skaber værdi for
+              dit projekt, vælg blandt en bred vifte af kompetencer</p>
+            <Search placeholder='søg kategori eller ' />
 
             <Button size="lg" className="button w-full sm:w-40 bg-gray-500 mt-10">
               <Link href="#events">
@@ -20,8 +20,15 @@ export default function Home() {
               </Link>
             </Button>
           </div>
-          <div className='mx-auto lg:w-1/2'>
-          <img className='float-right max-w-xs md:max-w-sm lg:max-w-md xl:max-w-lg' src="/assets/images/hero.jpg"   alt="hero-image"/>
+          <div className='  mx-auto lg:w-1/2   '>
+            <Image
+              src="/assets/images/hero.png"
+              alt="hero"
+              className='object-contain object-center  lg:object-right  max-h-[80vh]  2xl:max-h-[80vh]'
+              width={800}
+              height={1000}
+            />
+
           </div>
         </div>
       </section>
@@ -35,3 +42,8 @@ export default function Home() {
             height={800}
             className="max-h-[600px] object-contain object-center xl:object-right"
           /> */
+
+/**
+ *   <img className='float-right max-w-xs md:max-w-sm lg:max-w-md xl:max-w-lg' src="/assets/images/hero.png"   alt="hero-image"/>
+ * <div className='float-right mx-auto lg:w-1/2 max-w-xs md:max-w-sm lg:max-w-md xl:max-w-lg'>
+ */
